b6ecddfbe8ed0294dc5a62fc1e154c96759bbef3
NodjaLima <nodjamplima@gmail.com>
2022-10-13 18:45:43 -0300

    use relative path

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   vitefinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/ignite-lab-design-system/'
+      config.base = './'
     }
     return config
   }
